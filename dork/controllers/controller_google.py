12e74caf76c34d7d39ad8f736783e771caa5dd0c
a6888b <ayoubbrahim68@gmail.com>
2023-01-24 23:23:36 +0100

    add method abs

--- dork/controllers/controller_google.py
+++ dork/controllers/controller_google.py
@@ -1,20 +1,44 @@
-import requests
-import urllib3
-
 from .controller_base import ControllerBase
 from .controller_dork import ControllerDork
-# NOTE desactive le message d'attention ( comme on a mis a false a la verification de certification)
-urllib3.disable_warnings()
-
-
 
 class ControllerGoogle(ControllerBase, ControllerDork):
     def __init__(self, model, view):
         super().__init__(model, view)
+        self.navigator = 'chrome'
+        self.search_engine = 'google'
 
-    def file_type(self): pass
+        self.set_user_agent()
+        self.set_url()
 
-    def extension(self): pass
+
+    def set_user_agent(self) -> None:
+        """Ajoute un user_agent."""
+
+        self.user_agent = self.model.get_user_agent()
+
+
+    def set_url(self) -> None:
+        """Ajoute l'url on recuperer le lien de google."""
+
+        self.url = self.model.get_link_search()
+
+
+    def file_type(self, element: str): 
+        """Effectue une requete avec le mot clef filetype
+
+        Args:
+            element (str): element entrez par l'utilisateur pour la recherche
+        """
+
+        self.params['q'] = f'filetype: "{element}"' 
+        print(self.user_agent)
+        resp = self.get_resp()
+        
+        if resp.ok: 
+            print(resp.text)
+            
+    def extension(self): 
+        pass 
 
     def in_text(self): pass
 

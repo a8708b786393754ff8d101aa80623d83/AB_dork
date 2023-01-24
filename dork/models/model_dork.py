449865dc2c83f6bcf28af455634753d7c342eb15
a6888b <ayoubbrahim68@gmail.com>
2023-01-24 22:47:14 +0100

    add method abs

--- dork/models/model_dork.py
+++ dork/models/model_dork.py
@@ -10,10 +10,16 @@ class ModelDork(ABC):
     def get_link(self) -> str: pass
 
     @abstractmethod
+    def get_link_search(self) -> str: pass
+
+    @abstractmethod
     def get_title(self) -> str: pass
 
     @abstractmethod
     def get_all(self) -> list: pass
 
     @abstractmethod
-    def get_link_search(self) -> str: pass
+    def get_user_agent(self) -> str: pass
+
+    @abstractmethod
+    def get_url(self) -> str: pass

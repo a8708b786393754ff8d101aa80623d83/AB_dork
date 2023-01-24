2b1157a5a20524fb3f893d44eb8621980934c139
a6888b <ayoubbrahim68@gmail.com>
2023-01-24 23:26:35 +0100

    add object google

--- dork.py
+++ dork.py
@@ -1,31 +1,11 @@
 #! /usr/bin/python3
-import time
+from dork.controllers.controller_google import ControllerGoogle
 
-from dork.controllers.controller_google import GoogleDorkController
-from dork.controllers.scraping_controller import ScrapingController
+from dork.models.model_google import ModelGoogle
 
-from dork.models.model_google import ModelGoogleDork
-from dork.models.srcaping_model import ScrapingModel
+from dork.views.view_google import ViewGoogle
 
-from dork.views.view_base import ViewGoogleDork, ViewScraping
 
-start = time.perf_counter()
+google = ControllerGoogle(ModelGoogle(), ViewGoogle())
 
-extension_suffixe = {
-                    '(bdd|base de donée|database) ': 'https://www.file-extension.info/fr/fichiers-de-base-de-donnes/',
-                    '(system|windows|linux|javaOs|os)': 'https://www.file-extension.info/fr/fichiers-systme',
-                    '(compresse|compress|compresser|zip)': 'https://www.file-extension.info/fr/fichiers-compresss',
-                    '(texte|txt|text|script|file)': 'https://www.file-extension.info/text-files'
-                    }
-
-scrap = ScrapingController(ScrapingModel(), ViewScraping())
-google_dork_obj = GoogleDorkController(ModelGoogleDork(), ViewGoogleDork())
-
-if not scrap.already_registered(ScrapingModel.NAME_FILE_EXTENSION):  
-    scrap.save_suffix_extension(extension_suffixe)
-    
-if not google_dork_obj.already_registered(ModelGoogleDork.NAME_FILE_SAVING_ITEMS_GOOGLE_DORK): 
-    google_dork_obj.file_type()
-
-    
-print(time.perf_counter() - start) 
+google.file_type('pdf')

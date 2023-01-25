7ab0a3be1dad7b35cce35dc2f45746c773152856
a6888b <ayoubbrahim68@gmail.com>
2023-01-25 14:58:54 +0100

    delete attributs

--- dork/controllers/controller_google.py
+++ dork/controllers/controller_google.py
@@ -5,8 +5,6 @@ from .controller_dork import ControllerDork
 class ControllerGoogle(ControllerBase, ControllerDork):
     def __init__(self, model, view):
         super().__init__(model, view)
-        self.navigator = 'chrome'
-        self.search_engine = 'google'
 
         self.headers['Referer'] = 'https://www.google.com/'
         self.headers['Authority'] = 'www.google.com'

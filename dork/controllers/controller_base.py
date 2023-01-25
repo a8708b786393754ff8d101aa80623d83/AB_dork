080e08b435a805479c36ea4d2340f97b059aef82
a6888b <ayoubbrahim68@gmail.com>
2023-01-25 13:52:55 +0100

    add headers attribute and add headers and verify ssl certificate

--- dork/controllers/controller_base.py
+++ dork/controllers/controller_base.py
@@ -15,11 +15,18 @@ class ControllerBase(object):
     def __init__(self, model, view):
         self.model = model
         self.view = view
-        self.url = str
-        self.navigator = str
-        self.search_engine = str
-        self.user_agent = str
+        self.url = ''
+        self.navigator = ''
+        self.search_engine = ''
+        self.user_agent = ''
         self.params = {}
+        self.headers = {'User-agent': '',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                        'Accept-Language': 'en-US,en;q=0.5',
+                        'Accept-Encoding': 'gzip, deflate',
+                        'DNT': '1',
+                        'Connection': 'keep-alive',
+                        'Upgrade-Insecure-Requests': '1'}
 
     def get_resp(self) -> requests.Response:
         """Effectue une requetes en donnent les parametre est les user agent.
@@ -28,4 +35,4 @@ class ControllerBase(object):
             requests.Response: reponse de la requete
         """
 
-        return requests.get(self.url, params=self.params, headers={'User-agent': self.user_agent})
+        return requests.get(self.url, params=self.params, headers=self.headers, verify=True)

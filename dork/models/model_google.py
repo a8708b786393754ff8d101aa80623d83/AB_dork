fe24d7bad23c06d547e1ec415a8eb3b278277030
a6888b <ayoubbrahim68@gmail.com>
2023-01-25 14:57:53 +0100

    reformattage and add attribute

--- dork/models/model_google.py
+++ dork/models/model_google.py
@@ -3,35 +3,39 @@ from .model_dork import ModelDork
 
 import random
 
+
 class ModelGoogle(ModelBase, ModelDork):
     def __init__(self):
         super().__init__()
+        self.navigator = 'chrome'
+        self.search_engine = 'google'
 
-    def get_link_search(self) -> str: 
+    def get_link_search(self) -> str:
         """Recupere le lien de recherche de google
 
         Returns:
             str: lien de recherche de google
         """
 
-        data = self.get_content_file(self.const.PATH_DATA + self.const.FILENAME_LINKS)
-        return data['google']
-    
-    def get_user_agent(self) -> str: 
+        data = self.get_content_file(
+            self.const.PATH_DATA + self.const.FILENAME_LINKS)
+        return data[self.search_engine]
+
+    def get_user_agent(self) -> str:
         """Recupere un user agent 
 
         Returns:
             str: user agent
         """
 
-        data = self.get_content_file(self.const.PATH_DATA + self.const.FILENAME_USER_AGENT)
-        return data['chrome'][random.randint(0, len(data['chrome'])-1 )]
+        data = self.get_content_file(
+            self.const.PATH_DATA + self.const.FILENAME_USER_AGENT)
+        return data[self.navigator][random.randint(0, len(data[self.navigator])-1)]
 
     def blocks_request(self) -> bool: pass
-    
 
     def get_title(self) -> str: pass
 
     def get_all(self) -> list: pass
-    
+
     def get_link(self) -> list: pass

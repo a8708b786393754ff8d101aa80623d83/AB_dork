9085c654f4f19d0da74634ebcf25644ffbe0d04e
a6888b <ayoubbrahim68@gmail.com>
2023-01-26 22:57:49 +0100

    add method show params

--- dork/views/view_base.py
+++ dork/views/view_base.py
@@ -12,6 +12,14 @@ class ViewBase(object):
         BANNIERE
         """)
 
+    def params(self, params: dict):
+        query = ''
+        for key, value in params.items():
+            query += key + value
+
+        print(self.back.LIGHTBLUE_EX + self.fore.CYAN +
+              f'Query: {query}' + self.fore.RESET + self.back.RESET)
+
     def start_msg(self):
         print(self.back.YELLOW + self.fore.BLUE +
               'message' + self.fore.RESET + self.back.RESET)

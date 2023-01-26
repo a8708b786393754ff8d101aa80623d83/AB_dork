ee18b63253148cbd102f7ca2592f3a08ea991dd3
a6888b <ayoubbrahim68@gmail.com>
2023-01-26 22:38:49 +0100

    delete requirement

--- args.py
+++ args.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 
 def parser(): 
     arg = ArgumentParser()
-    arg.add_argument('-e', '--element', type=str, required=True, help='Enter the lement to search')
+    arg.add_argument('-e', '--element', type=str, help='Enter the lement to search')
     arg.add_argument('-f', '--file-type', type=str, help='Dork to filetype')
     arg.add_argument('-t', '--in-text', type=str, help='Dork to intext')
     arg.add_argument('-a', '--in-all-text', type=str, help='Dork to inallatext')

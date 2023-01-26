0ac11cb281943044b59dce6780a5eb4a9cceddd5
a6888b <ayoubbrahim68@gmail.com>
2023-01-26 21:54:51 +0100

    chnage action

--- args.py
+++ args.py
@@ -6,9 +6,9 @@ def parser():
     arg.add_argument('-f', '--file-type', type=str, help='Dork to filetype')
     arg.add_argument('-t', '--in-text', type=str, help='Dork to intext')
     arg.add_argument('-a', '--in-all-text', type=str, help='Dork to inallatext')
-    arg.add_argument('--google', type=bool, action='store_false', help='Google search use')
-    arg.add_argument('--bing', type=bool, action='store_false', help='Bing search use')
-    arg.add_argument('--duck-duck-go', type=bool, action='store_false', help='Duckduckgo search use')
+    arg.add_argument('--google', action='store_true', help='Google search use')
+    arg.add_argument('--bing', action='store_true', help='Bing search use')
+    arg.add_argument('--duck-duck-go', action='store_true', help='Duckduckgo search use')
 
 
     return arg.parse_args()

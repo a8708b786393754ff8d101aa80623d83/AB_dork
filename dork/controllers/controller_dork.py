0df4c73a253c4a44f7fd9ee39369f6eaf9a4a4c3
a6888b <ayoubbrahim68@gmail.com>
2023-01-29 20:08:19 +0100

    add dosctring

--- dork/controllers/controller_dork.py
+++ dork/controllers/controller_dork.py
@@ -1,22 +1,27 @@
-from abc import ABC
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 
 class ControllerDork(ABC):
+    """Controller des dork, c'est une classe abstraicte
+
+    Args:
+        ABC (object): class mere abs
+    """
+
     def __init__(self) -> None:
         super().__init__()
 
     @abstractmethod
-    def search(self, page: int = 1) -> None: pass
+    def search(self, page: int = 1) -> None: ...
 
     @abstractmethod
-    def file_type(self, element: str) -> None: pass
+    def file_type(self, element: str) -> None: ...
 
     @abstractmethod
-    def extension(self, element: str) -> None: pass
+    def extension(self, element: str) -> None: ...
 
     @abstractmethod
-    def in_text(self, element: str) -> None: pass
+    def in_text(self, element: str) -> None: ...
 
     @abstractmethod
-    def in_all_text(self, element: str) -> None: pass
+    def in_all_text(self, element: str) -> None: ...

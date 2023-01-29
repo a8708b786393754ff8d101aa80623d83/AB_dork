c70d96ac103679194efb364607e31b81be5288a8
a6888b <ayoubbrahim68@gmail.com>
2023-01-29 14:13:42 +0100

    add return type method

--- dork/views/view_google.py
+++ dork/views/view_google.py
@@ -6,13 +6,13 @@ class ViewGoogle(ViewBase, ViewDork):
     def __init__(self) -> None:
         super().__init__()
 
-    def title(self, title: str):
+    def title(self, title: str) ->None:
         print(self.fore.RED + f'[+*+]Title: {title}' + self.fore.RESET)
 
-    def link(self, link: str):
+    def link(self, link: str) ->None:
         print(self.fore.CYAN + f'[/-\]Link: {link}' + self.fore.RESET)
 
-    def user_agent(self, user_agent: str):
+    def user_agent(self, user_agent: str) ->None:
         print(self.fore.GREEN +
               f'[*-*]User agent: {user_agent}' + self.fore.RESET)
 

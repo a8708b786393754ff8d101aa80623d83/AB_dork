d5c798cd0807b5ce2abb6a082de4379a0a5ef82a
a6888b <ayoubbrahim68@gmail.com>
2023-01-29 20:30:19 +0100

    add doctring

--- dork/views/view_dork.py
+++ dork/views/view_dork.py
@@ -2,17 +2,23 @@ from abc import ABC, abstractmethod
 
 
 class ViewDork(ABC):
+    """View des dork, c'est une classe abstraicte
+
+    Args:
+        ABC (object): class mere abs
+    """
+
     def __init__(self) -> None:
         super().__init__()
 
     @abstractmethod
-    def url(self, url: str) -> None: pass
+    def url(self, url: str) -> None: ...
 
     @abstractmethod
-    def title(self, title: str) -> None: pass
+    def title(self, title: str) -> None: ...
 
     @abstractmethod
-    def link(self, link: str) -> None: pass
+    def link(self, link: str) -> None: ...
 
     @abstractmethod
-    def user_agent(self, user_agent: str) -> None: pass
+    def user_agent(self, user_agent: str) -> None: ...

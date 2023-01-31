3b979da511e425581120ec34afbc229320134ec8
a6888b <ayoubbrahim68@gmail.com>
2023-01-31 13:29:34 +0100

    add method, object for duckduckgo

---
+++ dork/models/model_duck_duck_go.py
@@ -0,0 +1,59 @@
+from .model_base import ModelBase
+from .model_dork import ModelDork
+
+import bs4
+
+
+class ModelDuckDuckGo(ModelBase, ModelDork):
+    """Classe model des donnée de duckduckgo.
+
+    Args:
+        ModelBase (object): model de base
+        ModelDork (object): model des dorks
+    """
+
+    def __init__(self) -> None:
+        """Methode constructrice."""
+
+        super().__init__()
+        self.navigator = 'duckduckgo'
+        self.search_engine = 'duckduckgo'
+
+
+    def get_link(self, div: bs4.element.Tag) -> str:
+        """Recupere le lien
+
+        Args:
+            div (bs4.element.Tag): balise div HTML
+
+        Returns:
+            str: lien de la balise a.
+        """
+
+        return div.h2.a['href']
+
+    def get_title(self, div: bs4.element.Tag) -> str:
+        """Recupere le titre
+
+        Args:
+            div (bs4.element.Tag): balise div HTML
+
+        Returns:
+            str: titre de la balise a.
+        """
+
+        return div.h2.a.span.text
+
+    def get_main_node(self, soup: bs4.BeautifulSoup) -> list[bs4.element.Tag]:
+        """Recupere le noeud main
+
+        Args:
+            soup (bs4.BeautifulSoup): soup de la page 
+
+        Returns:
+            list: données. 
+        """
+
+        return soup.find_all('article')
+
+    def blocks_request(self) -> bool: pass
---
+++ dork/views/view_duck_duck_go.py
@@ -0,0 +1,56 @@
+from .view_base import ViewBase
+from .view_dork import ViewDork
+
+
+class ViewDuckDuckGo(ViewBase, ViewDork):
+    """Classe View de l'affichage de recherche duckduckgo. 
+
+    Args:
+        ViewBase (object): view de base
+        ViewDork (object): view des dork
+    """
+
+    def __init__(self) -> None:
+        """Méthode constructrice."""
+
+        super().__init__()
+
+    def title(self, title: str) -> None:
+        """Affiche le titre
+
+        Args:
+            title (str): titre
+        """
+
+        print(self.fore.RED + f'[+*+]Title: {title}' + self.fore.RESET)
+
+    def link(self, link: str) -> None:
+        """Afficfhe un lien
+
+        Args:
+            link (str): lien
+        """
+
+        print(self.fore.CYAN + f'[/-\]Link: {link}' + self.fore.RESET)
+
+    def user_agent(self, user_agent: str) -> None:
+        """Affiche l'user agent 
+
+        Args:
+            user_agent (str): user agent 
+        """
+
+        print(self.fore.GREEN +
+              f'[*-*]User agent: {user_agent}' + self.fore.RESET)
+
+    def url(self, url: str) -> None:
+        """Affiche l'url de la requete
+
+        Args:
+            url (str): url
+        """
+
+        print(self.fore.YELLOW + f'[+*-*+]Url: {url} ' + self.fore.RESET)
+
+    
+

1d148421b8496902d96d108c6616c2197f9dd5d6
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-03-19 20:48:01 +0100

    add function get_proxy and renam proxy

--- dork/utils.py
+++ dork/utils.py
@@ -1,9 +1,10 @@
 import requests
 import base64
+import random
 from bs4 import BeautifulSoup
 
 
-def get_proxy(method: str = 'https') -> dict:
+def proxy(method: str = 'https') -> dict:
     """Recupere les proxies est leur port
 
     Args:
@@ -15,7 +16,7 @@ def get_proxy(method: str = 'https') -> dict:
 
     links = {'http': 'http://free-proxy.cz/fr/proxylist/country/all/http/ping/level1',
              'https': 'http://free-proxy.cz/fr/proxylist/country/all/https/ping/all/level1'}
-    data = {'ip': [], 'port': []}
+    data = {'proxy': []}
     if method in links:
 
         r = requests.get(links[method])
@@ -25,14 +26,29 @@ def get_proxy(method: str = 'https') -> dict:
         for tr in table:
             ip = tr.find('script')
             port = tr.find('span', class_='fport')
+            
             if port:
-                data['port'].append(port.text)
-
                 base_64_encode = ip.text.split(
                     'document.write(Base64.decode("')
                 ip_encode = base_64_encode[1].split('"))')[0]
                 bs64_byte = bytes(ip_encode, encoding='utf-8')
-                data['ip'].append(
-                    str(base64.b64decode(bs64_byte), encoding='utf-8'))
+                ip_decode = str(base64.b64decode(bs64_byte), encoding='utf-8')
+                data['proxy'].append(f'{method}://{ip_decode}:{port.text}')
 
     return data
+
+
+def get_proxy() -> dict:
+    data = {'http': '', 'https': ""}
+
+    ip_proxys_https = proxy()
+    ip_proxys_http = proxy('http')
+    
+    data['http'] = random.choice(ip_proxys_http['proxy'])
+    data['https'] = random.choice(ip_proxys_https['proxy'])
+    
+    print(data)
+    
+    return data 
+    
+get_proxy()

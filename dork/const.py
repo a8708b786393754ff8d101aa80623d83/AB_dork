66822e3995a825bc006673f4f1bfa1a2844f3386
a8708b786393754ff8d101aa80623d83 <74986761+a8708b786393754ff8d101aa80623d83@users.noreply.github.com>
2023-03-19 21:50:13 +0100

    delete const for file user agent

--- dork/const.py
+++ dork/const.py
@@ -3,6 +3,5 @@ PATH_DATA = 'data/'
 DORK = ['intext', 'inalltext', 'inurl', 'inallurl', 'filetype', 'site', 'blogurl', 'map', 'loc', 'inanchor', 'film', 'extension']
 
 FILENAME_HEADERS = 'headers.yml'
-FILENAME_USER_AGENT = 'user_agent.json'
 FILENAME_LINKS = 'links.json'
 FILENAME_DORK = 'operator.json'
